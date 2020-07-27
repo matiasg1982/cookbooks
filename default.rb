8c0ca78637cb2c6aa1095492b49de8e7017d889d
matiasg1982 <matiasg1982@gmail.com>
2020-07-27 15:12:10 -0300

    Create default.rb

---
+++ default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: lamp_stack
+# Recipe:: default
+#
+#
+
+execute "update-upgrade" do
+  command "sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' upgrade"
+  action :run
+end
